4b78f0e557509d929cede884aa8b223c4188cd1d
alina <degtyareva_18@mail.ru>
2026-09-24 17:00:53 +0300

    Remove Task Management project

--- Task Management/Task Management/Models/TaskPriority.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task_Management.Models
-{
-    public class TaskPriority
-    {
-        public int IdPriority { get; set; }
-        public string PriorityType { get; set; }  
-        public ICollection<CurrentTask> CurrentTasks { get; set; }
-    }
-}
--- Task Management/Task Management/Models/TaskStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task_Management.Models
-{
-    public class StatusTask
-    {
-        public int IdTaskStatus { get; set; }
-        public string StatusName { get; set; } 
-        public ICollection<CurrentTask> CurrentTasks { get; set; }
-    }
-}
